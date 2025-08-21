49be7e8bdd3a2a5f527991529e5bde50bf39bc45
sony-nurdianto <sonynurdianto445@gmail.com>
2025-08-22 06:37:30 +0700

    add observability to daemon service
    note need refactor from service-centric to event-centric

--- services/Events/auth/cmd/auth_farmer_ev_lis.go
+++ services/Events/auth/cmd/auth_farmer_ev_lis.go
@@ -11,16 +11,44 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
+	"github.com/sony-nurdianto/farm/services/Events/auth/internal/obs"
 	"github.com/sony-nurdianto/farm/services/Events/auth/internal/repo"
 	"github.com/sony-nurdianto/farm/services/Events/auth/internal/services"
 	"github.com/sony-nurdianto/farm/shared_lib/Go/kafkaev/avr"
 	"github.com/sony-nurdianto/farm/shared_lib/Go/kafkaev/kev"
 	"github.com/sony-nurdianto/farm/shared_lib/Go/kafkaev/schrgs"
+	"github.com/sony-nurdianto/farm/shared_lib/Go/observability/otel/logs"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/sony-nurdianto/farm/shared_lib/Go/observability"
 )
 
-func redisConnection() (c *redis.Client, err error) {
+func redisConnection(
+	ctx context.Context,
+	tracer trace.Tracer,
+	connMeter metric.Int64UpDownCounter,
+	errMetter metric.Int64Counter,
+) (c *redis.Client, err error) {
+	rdCtx, span := tracer.Start(ctx, "redis_connection",
+		trace.WithAttributes(
+			attribute.String("component", "redis"),
+			attribute.String("operation", "connect"),
+			attribute.String("master_name", os.Getenv("FARMER_REDIS_MASTER_NAME")),
+		),
+	)
+
+	defer span.End()
+
 	count := 0
 	for range 5 {
+		attemptSpan := trace.SpanFromContext(rdCtx)
+		attemptSpan.SetAttributes(attribute.Int("retry.attempt", count))
+
 		rdb := redis.NewFailoverClient(&redis.FailoverOptions{
 			MasterName:    os.Getenv("FARMER_REDIS_MASTER_NAME"),
 			SentinelAddrs: []string{os.Getenv("SENTINEL_FARMER_REDIS_ADDR")},
@@ -31,22 +59,102 @@ func redisConnection() (c *redis.Client, err error) {
 
 		_, err = rdb.Ping(context.Background()).Result()
 		if err == nil {
+			span.SetStatus(codes.Ok, "Redis connection successful")
+			span.SetAttributes(
+				attribute.Int("connection.attempts", count),
+				attribute.String("connection.status", "success"),
+			)
+
+			connMeter.Add(rdCtx, 1)
 			return rdb, nil
 		}
+
+		span.AddEvent("connection_attempt_failed",
+			trace.WithAttributes(
+				attribute.Int("attempt", count),
+				attribute.String("error", err.Error()),
+			),
+		)
 
 		time.Sleep(time.Second * 2)
 		count++
 	}
 
+	span.SetStatus(codes.Error, "Redis connection failed after all retries")
+	span.RecordError(err)
+	errMetter.Add(ctx, 1,
+		metric.WithAttributes(
+			attribute.String("error.type", "redis_connection_failed"),
+			attribute.String("component", "redis"),
+		),
+	)
+
 	return nil, fmt.Errorf("connect failed after %d attempts: %w", count, err)
 }
 
 func main() {
+	startTime := time.Now()
 	godotenv.Load()
-	rdb, err := redisConnection()
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	serviceObsName := "auth-farmer-event"
+
+	connColl, err := grpc.NewClient(
+		os.Getenv("OTELCOLLECTORADDR"),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+
+	obsProvider := observability.NewObservability(
+		serviceObsName,
+		connColl,
+	)
+
+	tp, mp, lp, err := obsProvider.Init(ctx)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	defer tp.Shutdown(ctx) // <- TraceProvider
+	defer mp.Shutdown(ctx) // <- MeterProvider
+	defer lp.Shutdown(ctx) // <-LogProvider
+
+	tracer := tp.Tracer(serviceObsName)
+	meter := mp.Meter(serviceObsName)
+	logger := logs.NewLogger()
+
+	observeMeter := obs.InitObserveMeter(
+		serviceObsName,
+		meter,
+	)
+
+	ctx, mainSpan := tracer.Start(ctx, "auth-service-daemon-lifecycle",
+		trace.WithAttributes(
+			attribute.String("service.name", serviceObsName),
+		),
+	)
+	defer mainSpan.End()
+
+	rdsCtx, rdSpan := tracer.Start(ctx, "initialize_redis_connection")
+	rdb, err := redisConnection(rdsCtx, tracer, observeMeter.RedisConnections, observeMeter.ErrCounter)
+	if err != nil {
+		observeMeter.ErrCounter.Add(
+			ctx, 1,
+			metric.WithAttributes(
+				attribute.String("service", serviceObsName),
+				attribute.String("description", "error when init redis connection"),
+				attribute.String("error_at", time.Now().Format(time.RFC3339)),
+			),
+		)
+
+		rdSpan.SetStatus(codes.Error, "Redis initialization failed")
+		rdSpan.RecordError(err)
+		logger.Error(rdsCtx, err.Error(), err)
+		return
+	}
+	rdSpan.SetStatus(codes.Ok, "Redis initialized successfully")
+	rdSpan.End()
 
 	cfgFarmer := map[kev.ConfigKeyKafka]string{
 		kev.BOOTSTRAP_SERVERS:             os.Getenv("KAKFKABROKER"),
@@ -64,29 +172,68 @@ func main() {
 		rdb,
 	)
 	if err != nil {
+		observeMeter.ErrCounter.Add(
+			ctx, 1,
+			metric.WithAttributes(
+				attribute.String("service", serviceObsName),
+				attribute.String("description", "error when crate NewRepo"),
+				attribute.String("error_at", time.Now().Format(time.RFC3339)),
+			),
+		)
 		log.Fatalln(err)
 	}
 
 	defer farmerSvcRepo.CloseRepo()
 
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	go func() {
 		farmerSvc := services.NewFarmerService(farmerSvcRepo)
-		if err := farmerSvc.SyncUserCache(ctx, "farmer-db.public.users_all_partitions"); err != nil {
+		if err := farmerSvc.SyncUserCache(
+			ctx, "farmer-db.public.users_all_partitions",
+			tracer,
+			meter,
+		); err != nil {
+			observeMeter.ErrCounter.Add(
+				ctx, 1,
+				metric.WithAttributes(
+					attribute.String("service", serviceObsName),
+					attribute.String("description", "error when runninc SyncUserCache service"),
+					attribute.String("error_at", time.Now().Format(time.RFC3339)),
+				),
+			)
 			log.Fatalln(err)
 		}
 	}()
 
 	fmt.Println("event service daemon run")
+	observeMeter.StartupDuration.Record(
+		ctx,
+		time.Since(startTime).Seconds(),
+		metric.WithAttributes(
+			attribute.String("service", serviceObsName),
+		),
+	)
+
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	uptimeStart := time.Now()
 
 	for {
 		select {
 		case <-ctx.Done():
 			fmt.Println("Event Server Stoping, Gracefully Stop ...")
+			observeMeter.RedisConnections.Add(ctx, -1)
 			fmt.Println("Application Quit.")
 			return
+		case <-ticker.C:
+			observeMeter.DaemonUpTime.Add(
+				ctx,
+				time.Since(uptimeStart).Seconds(),
+				metric.WithAttributes(
+					attribute.String("service", serviceObsName),
+				),
+			)
+
 		default:
 		}
 	}
